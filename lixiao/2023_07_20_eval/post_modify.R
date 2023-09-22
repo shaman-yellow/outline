790337ce6da4c90be8ad7d2c2ebaf12eabd19e4d
shaman-yellow <shaman.yellow@foxmail.com>
2023-09-23 06:50:04 +0800

    Sat Sep 23 06:50:04 AM CST 2023

--- lixiao/2023_07_20_eval/post_modify.R
+++
@@ -1,9 +0,0 @@
-# ==========================================================================
-# render as report
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-write_articlePdf("index.Rmd", "output.Rmd", "Report of Analysis\nauthor: 'Huang LiChuang of Wie-Biotech'")
-
-package_results()
-
-
