5d7adc18cb2f5b994ca8da6f3b89b24b1663896f
shaman-yellow <shaman.yellow@foxmail.com>
2023-09-27 18:12:41 +0800

    Wed Sep 27 06:12:41 PM CST 2023

--- lixiao/2023_09_27_dualdisease/post_modify.R
+++ lixiao/2023_09_27_dualdisease/post_modify.R
@@ -4,10 +4,16 @@
 
 write_articlePdf("index.Rmd", "output.Rmd", "Analysis\nauthor: 'Huang LiChuang of Wie-Biotech'")
 
-id <- "IN2023072801-3+销售：王双+客户：于骅+恶性肿瘤肌少症+2-3分"
+id <- "IN2023072803-3+销售：周燕青+客户：戴心怡+斑痕增生+生信分析"
 file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
 
 package_results(head = NULL, masterZip = NULL, report = report)
 file.rename("./client.zip", paste0(id, ".zip"))
 
+## for check
 
+"孙慧 <sunhui@wie-biotech.com>"
+"汪丽-业务进度 <wl@hzlxsw.com>"
+
+## final:
+"陈芳媛 <wengyuan@wie-biotech.com>"
