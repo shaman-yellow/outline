c8220feedfad685e0415261a5967ed287c7e5125
shaman-yellow <shaman.yellow@foxmail.com>
2023-10-07 06:50:04 +0800

    Sat Oct  7 06:50:04 AM CST 2023

---
+++ lixiao/2023_10_06_lunST/post_modify.R
@@ -0,0 +1,13 @@
+# ==========================================================================
+# render as report
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+write_articlePdf("index.Rmd", "output.Rmd", "Analysis\nauthor: 'Huang LiChuang of Wie-Biotech'")
+
+id <- "IN2023072803-3+销售：周燕青+客户：戴心怡+斑痕增生+生信分析"
+file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
+
+package_results(head = NULL, masterZip = NULL, report = report)
+file.rename("./client.zip", paste0(id, ".zip"))
+
+
