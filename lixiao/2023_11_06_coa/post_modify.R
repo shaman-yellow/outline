fbc8ef81928a6a6605cb72926982570afc57a3b0
shaman-yellow <shaman.yellow@foxmail.com>
2023-11-07 06:50:04 +0800

    Tue Nov  7 06:50:04 AM CST 2023

---
+++ lixiao/2023_11_06_coa/post_modify.R
@@ -0,0 +1,13 @@
+# ==========================================================================
+# render as report
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+write_articlePdf("index.Rmd", "output.Rmd", "Analysis\nauthor: 'Huang LiChuang of Wie-Biotech'")
+
+id <- "上海东方医院周久立-生信支持申请"
+file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
+
+package_results(head = NULL, masterZip = NULL, report = report)
+file.rename("./client.zip", paste0(id, ".zip"))
+
+
