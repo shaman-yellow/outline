1913b7c89094b2a53c84d867deec7babd7030a8c
shaman-yellow <shaman.yellow@foxmail.com>
2023-11-18 06:50:06 +0800

    Sat Nov 18 06:50:06 AM CST 2023

--- lixiao/2023_10_12_dr/post_modify.R
+++ lixiao/2023_10_12_dr/post_modify.R
@@ -4,7 +4,7 @@
 
 write_articlePdf("index.Rmd", "output.Rmd", "")
 
-id <- "IN2023072803-3+销售：周燕青+客户：戴心怡+斑痕增生+生信分析"
+id <- "IN2023110603-3+销售：吴航贵+客户：袁路+糖尿病视网膜病变+生信分析3-4分"
 file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
 
 package_results(head = NULL, masterZip = NULL, report = report)
