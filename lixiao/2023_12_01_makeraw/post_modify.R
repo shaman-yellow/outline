a9eb87d5fbb69593849bb09ff418160646a8c65f
shaman-yellow <shaman.yellow@foxmail.com>
2023-12-02 06:50:04 +0800

    Sat Dec  2 06:50:04 AM CST 2023

---
+++ lixiao/2023_12_01_makeraw/post_modify.R
@@ -0,0 +1,25 @@
+# ==========================================================================
+# render as report
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+write_articlePdf("index.Rmd", "output.Rmd", "")
+
+id <- "IN2023072803-3+销售：周燕青+客户：戴心怡+斑痕增生+生信分析"
+file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
+
+package_results(head = NULL, masterZip = NULL, report = report)
+file.rename("./client.zip", paste0(id, ".zip"))
+
+info <- items(
+  belong = odate(6),
+  type = "固定业务",
+  title = od_get_title(),
+  status = "完成",
+  coef = .25,
+  date = "2023-06-29",
+  info = od_get_info(),
+  id = od_get_id(),
+  receive_date = od_get_date(),
+  score = od_get_score(),
+  member = "黄礼闯"
+)
