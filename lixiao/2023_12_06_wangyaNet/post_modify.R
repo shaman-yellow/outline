6da301615577a887eff9af63f5a894f050a3cecd
shaman-yellow <shaman.yellow@foxmail.com>
2023-12-08 06:50:06 +0800

    Fri Dec  8 06:50:06 AM CST 2023

--- lixiao/2023_12_06_wangyaNet/post_modify.R
+++ lixiao/2023_12_06_wangyaNet/post_modify.R
@@ -4,7 +4,7 @@
 
 write_articlePdf("index.Rmd", "output.Rmd", "")
 
-id <- "半夏泻心汤网络药理学分析"
+id <- odb("name", "analysis")
 file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
 
 package_results(head = NULL, masterZip = NULL, report = report)
@@ -13,12 +13,12 @@ file.rename("./client.zip", paste0(id, ".zip"))
 info <- items(
   belong = odate(12),
   type = "其他业务",
-  title = "半夏泻心汤网络药理学分析",
+  title = odb("name", "analysis"),
   status = "完成",
   coef = NA,
   date = "2023-12-06",
   info = od_get_info(),
-  id = od_get_id(),
+  id = id,
   receive_date = od_get_date(),
   score = od_get_score(),
   member = "黄礼闯"
---
+++ lixiao/2023_12_07_fanghejing2/post_modify.R
@@ -0,0 +1,25 @@
+# ==========================================================================
+# render as report
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+write_articlePdf("index.Rmd", "output.Rmd", "")
+
+id <- odb("name", "analysis")
+file.copy("./output.pdf", report <- paste0(id, ".pdf"), T)
+
+package_results(head = NULL, masterZip = NULL, report = report)
+file.rename("./client.zip", paste0(id, ".zip"))
+
+info <- items(
+  belong = odate(12),
+  type = "其他业务",
+  title = odb("name", "analysis"),
+  status = "完成",
+  coef = NA,
+  date = "2023-12-08",
+  info = od_get_info(),
+  id = id,
+  receive_date = od_get_date(),
+  score = od_get_score(),
+  member = "黄礼闯"
+)
