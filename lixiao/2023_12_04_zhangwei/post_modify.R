bb53eca08f4f6888e6657895f4f878c83dc9fd7c
shaman-yellow <shaman.yellow@foxmail.com>
2023-12-26 06:50:02 +0800

    Tue Dec 26 06:50:02 AM CST 2023

--- lixiao/2023_12_04_zhangwei/post_modify.R
+++ lixiao/2023_12_04_zhangwei/post_modify.R
@@ -15,7 +15,7 @@ info <- items(
   type = "其他业务",
   title = "RNA-seq 探究 rTMS 对 SCI 和 NP 的影响",
   status = "完成",
-  coef = "1.5-2",
+  coef = .25,
   date = "2023-12-12",
   info = od_get_info(),
   id = od_get_id(),
