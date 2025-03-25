079cf0cb8f165e476781928c1a6c754dbe99da1c
shaman-yellow <shaman.yellow@foxmail.com>
2025-03-25 08:30:05 +0800

    Tue Mar 25 08:30:05 AM CST 2025

---
+++ bosai/2024_11_07_BSZD231122_Q1gurouliu/markers.R
@@ -0,0 +1,15 @@
+
+cell_markers <- list(
+  Osteoblastic_OS = c("COL1A1", "CDH11", "RUNX2"),
+  Proliferating_osteoblastic_OS = c("COL1A1", "CDH11", "RUNX2", "TOP2A", "PCNA", "MKI67"),
+  Chondroblastic_OS = c("ACAN", "COL2A1", "SOX9"),
+  Osteoclastic = c("CTSK", "MMP9"),
+  T_NK_cells = c("IL7R", "CD3D", "NKG7"),
+  Myeloid = c("CD74", "CD14", "FCGR3A"),
+  Fibroblasts = c("COL1A1", "LUM", "DCN"),
+  Pericytes = c("ACTA2", "RGS5"),
+  MSCs = c("CXCL12", "SFRP2", "MME"),
+  Myoblasts = c("MYLPF", "MYL1"),
+  Endothelial = c("PECAM1", "VWF")
+)
+
